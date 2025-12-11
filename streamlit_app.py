2b09c7c411d5576b196da24137a46ad25dd5e3d5
nicdims0111-well <nicdims0111@gmail.com>
2025-12-11 15:35:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title("This is a title")
 st.title(":blue[PENENTUAN BILANGAN PANGKAT] :sunglasses:")
+
+
+number = st.number_input("Insert a number")
+st.write("The current number is ", number)
