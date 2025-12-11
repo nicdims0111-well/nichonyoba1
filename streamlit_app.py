d76af5b22c5582767e9d7e19091df8c6159657ad
nicdims0111-well <nicdims0111@gmail.com>
2025-12-11 15:29:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,21 @@ st.write(
 
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
 st.header("These headers have rotating dividers", divider=True)
 st.header("One", divider=True)
 st.header("Two", divider=True)
