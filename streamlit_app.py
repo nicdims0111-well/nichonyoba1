742aed8499a739dafa0bffce8624b1200b82e9fe
nicdims0111-well <nicdims0111@gmail.com>
2025-12-11 15:32:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,40 +2,6 @@ import streamlit as st
 
 st.write("Hello, :violet[*MOKKK!*] :smile:")
 
-
-
-import pandas as pd
-
-st.write(1234)
-st.write(
-    pd.DataFrame(
-        {
-            "first column": [1, 2, 3, 4],
-            "second column": [10, 20, 30, 40],
-        }
-    )
-)
-
-
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header( :blue[PENENTUAN PANGKAT] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
 
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
