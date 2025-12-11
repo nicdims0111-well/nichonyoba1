531813ad9be0da2eee464ed3f2dc97a021d1c959
nicdims0111-well <nicdims0111@gmail.com>
2025-12-11 15:41:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ st.title("This is a title")
 st.title(":blue[PENENTUAN BILANGAN PANGKAT] :sunglasses:")
 
 
-number = st.number_input("Insert a number")
-st.write("The current number is ", number)
+number = int(st.number_input("Insert a number", min_value=0, max_value=))
+if number%2==1:
+  st.write('bilangan', number, 'termasuk bilangan ganjil')
+else:
+  st.write('bilangan', number, 'termasuk bilangan genap')
